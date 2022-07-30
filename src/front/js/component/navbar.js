7322c0d28cd00f8865fdf8a2636e4e6dd7080362
b5720 <baez5720@outlook.com>
2022-07-30 08:28:55 +0000

    se agrega nueva version de botstrap al template.html

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "../../styles/home.css";
+
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-expand-lg bg-light">
-			<div className="container-fluid">
-				<a className="navbar-brand" href="#">Navbar</a>
+		<nav className="navbar navbar-expand-lg my-navbar navbar-dark bg-dark">
+			<div className="container">
+				<a className="navbar-brand" href="#">Da Room's</a>
 				<button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 					<span className="navbar-toggler-icon"></span>
 				</button>
@@ -15,11 +17,14 @@ export const Navbar = () => {
 							<a className="nav-link active" aria-current="page" href="#">Home</a>
 						</li>
 						<li className="nav-item">
-							<a className="nav-link" href="#">Link</a>
+							<a className="nav-link" href="#">WomensWear</a>
 						</li>
 
 						<li className="nav-item">
-							<a className="nav-link disabled">Disabled</a>
+							<a className="nav-link">MensWear</a>
+						</li>
+						<li className="nav-item">
+							<a className="nav-link">Sneakers</a>
 						</li>
 					</ul>
 					<form className="d-flex" role="search">
@@ -31,6 +36,3 @@ export const Navbar = () => {
 		</nav>
 	);
 };
-
-
-
