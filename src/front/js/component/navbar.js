93d958e278c4ab52472e5dce5969ab63866d207c
b5720 <baez5720@outlook.com>
2022-07-30 13:40:37 +0000

    Se hicieron cambios en el Navbar

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -5,34 +5,36 @@ import "../../styles/home.css";
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-expand-lg my-navbar navbar-dark bg-dark">
-			<div className="container">
-				<a className="navbar-brand" href="#">Da Room's</a>
+
+		<nav className="navbar navbar-expand-lg sticky-top my-navbar">
+			<div className="container-fluid">
+				<a className="navbar-brand" href="#">DA ROOM'S</a>
 				<button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 					<span className="navbar-toggler-icon"></span>
 				</button>
 				<div className="collapse navbar-collapse" id="navbarSupportedContent">
 					<ul className="navbar-nav me-auto mb-2 mb-lg-0">
 						<li className="nav-item">
-							<a className="nav-link active" aria-current="page" href="#">Home</a>
-						</li>
-						<li className="nav-item">
 							<a className="nav-link" href="#">WomensWear</a>
 						</li>
-
 						<li className="nav-item">
-							<a className="nav-link">MensWear</a>
+							<a className="nav-link" href="#">MensWear</a>
 						</li>
-						<li className="nav-item">
-							<a className="nav-link">Sneakers</a>
+							<li className="nav-item">
+							<a className="nav-link" href="#">Sneakers</a>
 						</li>
 					</ul>
 					<form className="d-flex" role="search">
 						<input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"></input>
-						<button className="btn btn-outline-success" type="submit">Search</button>
+						<button className="btn btn-outline-secondary" type="submit"><i class="bi bi-search fa-lg"></i></button>
 					</form>
+					<div className="botones-user-cart">
+						<button type="button" class="btn btn-outline-secondary"><i class="bi bi-person-circle fa-lg"></i></button>
+						<button type="button" class="btn btn-outline-secondary"><i class="bi bi-bag-heart fa-lg"></i></button>
+					</div>
 				</div>
 			</div>
 		</nav>
+
 	);
 };
