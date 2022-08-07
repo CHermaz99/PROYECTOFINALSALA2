fc441de1266024451be84ed0e03cd415b513718a
b5720 <baez5720@outlook.com>
2022-08-07 01:06:31 +0000

    Se suben los cambios del models.py

--- src/api/models.py
+++ src/api/models.py
@@ -13,13 +13,24 @@ class User(db.Model):
 
 class Product(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    photo_1 = db.Column(db.String(300), unique=False, nullable=False)
-    photo_2 = db.Column(db.String(300), unique=False, nullable=False)
-    name = db.Column(db.String(120), unique=True, nullable=False)
-    description = db.Column(db.String(300), unique=False, nullable=False)
+    name = db.Column(db.String(120), unique=True, nullable=False)  
+    description = db.Column(db.String(250))
+    stock = db.Column(db.Integer)
+    is_active = db.Column(db.Boolean, default=True)
+    image = db.Column(db.String(250))
+    price = db.Column(db.Float)
+
+    def __repr__(self):
+        return f'<Product {self.name}>'
+
+    # >> Comienza el comentario del class Product(db.Model) siguiendo el video de Marcos no veo ésto necesario <<
+
+    #photo_2 = db.Column(db.String(300), unique=False, nullable=False)
     #price = db.Column(db.Float)
     #size = db.Column(db.String(80), unique=False, nullable=False)
-    category_id = (db.Integer, ForeignKey(category.id))
+    #category_id = (db.Integer, ForeignKey(category.id))
+
+    # >> Termina el comentario del class Product(db.Model) <<
 
     
 class Category(db.Model):
