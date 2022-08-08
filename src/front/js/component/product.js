4be29683ac907567eeb1780f89957f3a94182a4d
b5720 <baez5720@outlook.com>
2022-08-08 02:09:11 +0000

    se agregan los ultimos cambios

--- src/front/js/component/product.js
+++ src/front/js/component/product.js
@@ -8,7 +8,9 @@ const Product = (props) => {
         <div className="card-body">
             <h5 className="card-title">{props.name}</h5>
             <p className="card-text">Price: {props.price}$</p>
-            <Link to={`/product/${props.id}`} className="btn btn-primary">Detail</Link>
+            <Link to={`/product/${props.id}`} className="btn btn-primary">
+                Detail
+            </Link>
         </div>
   </div>
 }
--- src/front/js/pages/CategoryWomensWare.js
+++ src/front/js/pages/CategoryWomensWare.js
@@ -3,23 +3,25 @@ import { Context } from "../store/appContext";
 import "../../styles/index.css";
 import Product from "../component/product";
 
-export const womensware = () => {
+export const CategoryWomensWare = () => {
     const { store } = useContext(Context);
 
     return (
-        <div className="row">
-            {
-                store.products.map((product)=>{
-                    return <div className="col-12 col-md-4">
-                        <Product 
-                            id={product.id} 
-                            name={product.name} 
-                            price={product.price}
-                            image={product.image}
-                        />
-                    </div>
-                })
-            }
+        <div className="container">
+            <div className="row">
+                {store.products.map((product) => {
+                    return (
+                        <div className="col-12 col-md-4">
+                            <Product 
+                                id={product.id} 
+                                name={product.name} 
+                                price={product.price}
+                                image={product.image}
+                            />
+                        </div>
+                    );
+                })}
+            </div>
         </div>
-    )
+    );
 }
