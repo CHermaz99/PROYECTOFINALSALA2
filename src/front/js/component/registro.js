ce701291d56334d46d78cd4a5c947e1fc57036df
CHermaz99 <cesarhermaz@gmail.com>
2022-08-08 16:00:09 +0000

    pagina registro

--- src/front/js/component/registro.js
+++ src/front/js/component/registro.js
@@ -2,13 +2,115 @@ import React from "react";
 
 const Registro = () => {
   return (
-    <ul className="list-group">
-      <li className="list-group-item">An item</li>
-      <li className="list-group-item">A second item</li>
-      <li className="list-group-item">A third item</li>
-      <li className="list-group-item">A fourth item</li>
-      <li className="list-group-item">And a fifth one</li>
-    </ul>
+    <div className="container col-8">
+      <br></br>
+      <br></br>
+      <div className="container col-6">
+        <h1>UNETE AL CLUB</h1>
+      </div>
+      <br></br>
+      <br></br>
+      <div className="input-group input-group-lg">
+        <span className="input-group-text" id="inputGroup-sizing-lg">
+          Name
+        </span>
+        <input
+          type="text"
+          className="form-control"
+          aria-label="Sizing example input"
+          aria-describedby="inputGroup-sizing-lg"
+        />
+      </div>
+      <br></br>
+      <div className="input-group input-group-lg">
+        <span className="input-group-text" id="inputGroup-sizing-lg">
+          Phone Number
+        </span>
+        <input
+          type="text"
+          className="form-control"
+          aria-label="Sizing example input"
+          aria-describedby="inputGroup-sizing-lg"
+        />
+      </div>
+      <br></br>
+      <div className="input-group input-group-lg">
+        <span className="input-group-text" id="inputGroup-sizing-lg">
+          Email
+        </span>
+        <input
+          type="text"
+          className="form-control"
+          aria-label="Sizing example input"
+          aria-describedby="inputGroup-sizing-lg"
+        />
+      </div>
+      <br></br>
+      <div className="input-group input-group-lg">
+        <span className="input-group-text" id="inputGroup-sizing-lg">
+          Address
+        </span>
+        <input
+          type="text"
+          className="form-control"
+          aria-label="Sizing example input"
+          aria-describedby="inputGroup-sizing-lg"
+        />
+      </div>
+      <br></br>
+      <div className="input-group input-group-lg">
+        <span className="input-group-text" id="inputGroup-sizing-lg">
+          Pass
+        </span>
+        <input
+          type="text"
+          className="form-control"
+          aria-label="Sizing example input"
+          aria-describedby="inputGroup-sizing-lg"
+        />
+      </div>
+      <br></br>
+      <div className="input-group input-group-lg">
+        <span className="input-group-text" id="inputGroup-sizing-lg">
+          Confirm Pass
+        </span>
+        <input
+          type="text"
+          className="form-control"
+          aria-label="Sizing example input"
+          aria-describedby="inputGroup-sizing-lg"
+        />
+      </div>
+      <br></br>
+      <div className="input-group input-group-lg">
+        <span className="input-group-text" id="inputGroup-sizing-lg">
+          Large
+        </span>
+        <input
+          type="text"
+          className="form-control"
+          aria-label="Sizing example input"
+          aria-describedby="inputGroup-sizing-lg"
+        />
+      </div>
+      <br></br>
+      <br></br>
+      <div className="container-fluid col-6">
+        <button type="button" className="btn btn-light btn-lg">
+          CONTINUE
+        </button>
+        <button type="button" className="btn btn-light btn-lg">
+          SIGN IN
+        </button>
+      </div>
+      <div className="container-fluid col-4">
+        <img
+          src="https://m.media-amazon.com/images/I/41mx67OkorS._AC_SL1500_.jpg"
+          className="d-block w-100"
+          alt="..."
+        />
+      </div>
+    </div>
   );
 };
 
---
+++ src/front/js/pages/paginaRegistro.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Registro from "../component/registro";
+
+export const PaginaRegistro = () => {
+  return (
+    <div className="container">
+      <div className="row">
+        <Registro />
+      </div>
+    </div>
+  );
+};
+
+export default PaginaRegistro;
