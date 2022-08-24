e43b7435364c27fa8730483ee28ab9d0c8996a36
CHermaz99 <cesarhermaz@gmail.com>
2022-08-24 19:14:57 +0000

    vacio componente personal

--- src/front/js/component/personal.js
+++ src/front/js/component/personal.js
@@ -1,52 +1,14 @@
 import React, { useState, useContext } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { Context } from "../store/appContext";
+import "../../styles/personal.css";
 
 const Personal = () => {
   const navigate = useNavigate();
   const { store, actions } = useContext(Context);
   const [text, setText] = useState({});
 
-  return (
-    <div className="register-photo">
-      <div className="form-container">
-        <div className="image-holder"></div>
-
-        <form
-          className="form-control"
-          onSubmit={(event) => {
-            event.preventDefault();
-            console.log(text);
-            actions.create_user(text).then(() => {
-              navigate("/");
-            });
-          }}
-        >
-          <br></br>
-          <br></br>
-          <h2 className="text-center">
-            <strong>Bienvenid@</strong>
-            <br></br>
-            <br></br>a Da Room's! Area personal
-          </h2>
-
-          <br></br>
-          <br></br>
-          <div className="form-group">
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
-          </div>
-        </form>
-      </div>
-    </div>
-  );
+  return <h1>Hola</h1>;
 };
 
 export default Personal;
