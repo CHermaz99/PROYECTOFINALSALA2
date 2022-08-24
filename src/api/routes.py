28abf9f5d6d8479c8be97d3d36ecb39b69c6adc6
CHermaz99 <cesarhermaz@gmail.com>
2022-08-24 16:43:17 +0000

    Recuperado importación Flask jwt

--- src/api/routes.py
+++ src/api/routes.py
@@ -4,6 +4,8 @@ This module takes care of starting the API Server, Loading the DB and Adding the
 from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User, Product, Category
 from api.utils import generate_sitemap, APIException
+from flask_jwt_extended import create_access_token, get_jwt_identity, jwt_required
+
 
 api = Blueprint('api', __name__)
 
@@ -99,4 +101,4 @@ def get_user(id):
 
 
     
-
+#MIOOOOOOO
