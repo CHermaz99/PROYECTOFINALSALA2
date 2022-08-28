f5239f92bf694584196754205d7dc328f8c19ee7
CHermaz99 <cesarhermaz@gmail.com>
2022-08-28 18:55:49 +0000

    se ponen links en footer

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -12,32 +12,40 @@ export const Footer = () => (
           </Link>
         </div>
       </div>
-      <div id="address">
-         Calle Gran Vía 12, 28002, MADRID
-        </div>
-        <div id="copy">
-          Copyright@DaRooms 2022
-        </div>
+      <div id="address">Calle Gran Vía 12, 28002, MADRID</div>
+      <div id="copy">Copyright@DaRooms 2022</div>
       <div className="row align-items-center">
         <div className="col">
-          <button type="button" className="btn btn-dark ">
-            <i id="icon" className="bi bi-facebook"></i>
-          </button>
-          <button type="button" className="btn btn-dark">
-            <i id="icon" className="bi bi-instagram"></i>
-          </button>
-          <button type="button" className="btn btn-dark">
-            <i id="icon" className="bi bi-twitter"></i>
-          </button>
-          <button type="button" className="btn btn-dark">
-            <i id="icon" className="bi bi-whatsapp"></i>
-          </button>
-          <button type="button" className="btn btn-dark">
-            <i id="icon" className="bi bi-google"></i>
-          </button>
-          <button type="button" className="btn btn-dark">
-            <i id="icon" className="bi bi-envelope"></i>
-          </button>
+          <a href="https://www.facebook.com" target="_blank" rel="noreferrer">
+            <button type="button" className="btn btn-dark ">
+              <i id="icon" className="bi bi-facebook"></i>
+            </button>
+          </a>
+          <a href="https://www.instagram.com" target="_blank" rel="noreferrer">
+            <button type="button" className="btn btn-dark">
+              <i id="icon" className="bi bi-instagram"></i>
+            </button>
+          </a>
+          <a href="https://twitter.com" target="_blank" rel="noreferrer">
+            <button type="button" className="btn btn-dark">
+              <i id="icon" className="bi bi-twitter"></i>
+            </button>
+          </a>
+          <a href="https://www.whatsapp.com" target="_blank" rel="noreferrer">
+            <button type="button" className="btn btn-dark">
+              <i id="icon" className="bi bi-whatsapp"></i>
+            </button>
+          </a>
+          <a href="https://google.com" target="_blank" rel="noreferrer">
+            <button type="button" className="btn btn-dark">
+              <i id="icon" className="bi bi-google"></i>
+            </button>
+          </a>
+          <a href="https://www.gmail.com" target="_blank" rel="noreferrer">
+            <button type="button" className="btn btn-dark">
+              <i id="icon" className="bi bi-envelope"></i>
+            </button>
+          </a>
         </div>
       </div>
     </div>
