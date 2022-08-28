d678ad2e5ff49782b327dc9de0f35f96d4ec4695
b5720 <baez5720@outlook.com>
2022-08-28 17:35:29 +0000

    se modifico nuevamente el footer

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -3,7 +3,7 @@ import { Link, useNavigate } from "react-router-dom";
 import "../../styles/index.css";
 
 export const Footer = () => (
-  <footer className="footer mt-auto py-3 text-center">
+  <footer className="footer mt-auto text-center">
     <div className="container-fluid bg-dark text-white text-center">
       <div className="row align-items-start">
         <div className="darooms">
@@ -12,12 +12,8 @@ export const Footer = () => (
           </Link>
         </div>
       </div>
-      <div id="address">
-         Calle Gran Vía 12, 28002, MADRID
-        </div>
-        <div id="copy">
-          Copyright@DaRooms 2022
-        </div>
+      <div id="address">Calle Gran Vía 12, 28002, MADRID</div>
+      <div id="copy">Copyright@DaRooms 2022</div>
       <div className="row align-items-center">
         <div className="col">
           <button type="button" className="btn btn-dark ">
